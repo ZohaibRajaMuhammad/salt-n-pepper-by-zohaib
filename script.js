b9895f0c217ebfd3cea0835d4ea22816bcb493b5
Muhammad Zohaib <rajamuhammadzohaib99@gmail.com>
2024-09-01 20:28:10 +0500

    Salt n'pepper  clone

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+//sliderend
+
+    function toggleMenu() {
+        const navMenu = document.querySelector('.nav-menu');
+        const navElements = document.querySelector('.nav-elements');
+        const franchisesBtn = document.querySelector('.franchises-btn');
+        navMenu.classList.toggle('active');
+        navElements.classList.toggle('active');
+        franchisesBtn.classList.toggle('active');
+    }
